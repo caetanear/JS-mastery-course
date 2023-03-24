6b6d1aa3590e7864e5c1f179eba0e4179eec67d7
J. M <juliasoriju@gmail.com>
2023-03-24 10:46:45 -0300

    24.03

--- vacation destination list/script.js
+++ vacation destination list/script.js
@@ -13,14 +13,14 @@ function handleFormSubmit(event){
     var destDesc = event.target.elements['description'].value;
 
     //2. clear out the form fields
-    for (var i=0; i<detailsForm.length; i++){
+    for (var i=0; i < detailsForm.length; i++){
         detailsForm.elements[i].value = "";
 
     }
 
     
     //4. if needed, change the header at the top of the destination list
-    var wishListContainer = document.getElementById('destinations_container');
+    var wishListContainer = document.getElementById("#destinations_container");
 
     if(wishListContainer.children.length === 0){
         document.getElementById('title').innerHTML = "My wish list";
@@ -33,4 +33,35 @@ function handleFormSubmit(event){
     function createDestinationCard(name, location, photoURL, description){
         var card = document.createElement('div');
         card.className = "card";
+
+        var img = document.createElement('img');
+        img.setAttribute('alt', name);
+
+        var constantPhotoURL = "images/signpost.jpg";
+
+        if(photoURL.length === 0){
+            img.setAttribute('src', constantPhotoURL);
+        }
+        else{
+            img.setAttribute('src', photoURL);
+
+        }
+
+        var cardBody = document.createElement("div");
+            cardBody.className = "card-body";
+
+        var cardTitle = document.createElement("h3");
+            cardTitle.innerText = name;
+            cardBody.appendChild(cardTitle);
+        
+        var cardSubtitle = document.createElement("h4");
+            cardSubtitle.innerText = location;
+            cardBody.appendChild(cardSubtitle);
+
+        if(description.length !== 0){
+            var cardText = document.createElement("p");
+            cardText.className = "card-text";
+            cardText.innerText = description;
+            cardBody.appendChild(cardText);
+        }
     }
